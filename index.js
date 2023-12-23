e75f141574321737e3d1e767b72e8a633174e630
Yago Pérez Molanes <72870696+alu0101254678@users.noreply.github.com>
2023-12-23 10:14:19 +0000

    Modified connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+let messagge = "";
+
 // Conexión a la base de datos de MongoDB en Azure Cosmos DB
 const mongoURI = "mongodb://alu0101254678:Ivlc6PhBZ9i71tj7Q5Jx8b39ZV4b4yccm2Hgwste6YYGcLl3hUSv5YxrNrAiEoUT8UG5fGd2OFeVACDbdeOomg%3D%3D@alu0101254678.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@alu0101254678@";
 mongoose.connect(mongoURI, {
@@ -17,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 // Ruta principal para verificar la conexión y realizar la conexión a la base de datos
 app.get('/', (req, res) => {
-    res.send('Bienvenido a la API de Books Shopping');
+    res.send(messagge);
 });
 
 app.listen(PORT, () => {
